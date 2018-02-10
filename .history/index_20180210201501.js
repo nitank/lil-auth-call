ffba0aa6ca19030fc6fd1f87bca1ee6a7e4ee3b3
nitank <aggarwal.nitank@gmail.com>
2018-02-10 20:38:59 +0530

    first commit

---
+++ .history/index_20180210201501.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const { URL } = require('url');
+const { normalize } = require('path');
+const lilAuthUrl = process.env.LIL_AUTH_URL || 'http://lil-auth';
+console.log('LIL_AUTH_URL', lilAuthUrl);
+
+const { hostname, port, pathname = '' } = new URL(lilAuthUrl);
+const path = normalize(`${pathname}/auth/validate`);
+
+const myPath = `${lilAuthUrl}${path}`;
+console.log('myPath', myPath);
+
+const auth = () => fn => async function (req, res) {
+  const token = req.headers.authorization;
+  if (token) {
+    console.log('found token');
+    const { data } = await axios.post(`${lilAuthUrl}${path}`, { token });
+    // console.log('data', data);
+    req.accessToken = data;
+    console.log('user from token', data.user.id);
+  }
+  return await fn(req, res);
+};
+
+exports.authCall = async function authCall(fileName) {
+  return await axios.post(`${lilAuthUrl}${path}`, { token });
+};
+
+module.exports = auth;
